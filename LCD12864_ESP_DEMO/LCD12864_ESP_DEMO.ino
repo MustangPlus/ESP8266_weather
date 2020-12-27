619b1cd23698b01da09ab735aa2f5218decea2e5
MustangPlus <1836060154@qq.com>
2020-12-27 19:01:01 +0800

    天气更新时间设置完成 无法加入定时中断，进入中断无法连接到服务器 使用millis而不是delay的原因是，delay是定时中断，占用芯片资源，易使芯片跑飞

--- LCD12864_ESP_DEMO/LCD12864_ESP_DEMO.ino
+++ LCD12864_ESP_DEMO/LCD12864_ESP_DEMO.ino
@@ -2,6 +2,7 @@
 #include <U8g2lib.h>
 #include <DHT.h>
 #include <ESP8266WiFi.h>
+#include <Ticker.h>
 
 #ifdef U8X8_HAVE_HW_SPI
 #include <SPI.h>
@@ -31,23 +32,51 @@
                 //&language=，语言
                 //&start=0&days=1，预报时间
                 //api.seniverse.com，心知天气网站
-                
+#define weatherUpdateTime 10000 //更新天气预报时间，单位ms
+
 char* host = "api.seniverse.com";
+int lastTime = 0;
 
 WiFiClient client;//构造/类实例？C++内容
+
+Ticker flipper;
 
 U8G2_ST7567_OS12864_F_4W_SW_SPI u8g2(U8G2_R2, /* clock=*/ 14, /* data=*/ 13, /* cs=*/ 15, /* dc=*/ 4, /* reset=*/ 5); //构造/类实例？C++内容 
 
 DHT dht(DHTPIN, DHTTYPE);//构造/类实例？C++内容
 
-bool sendRequest(char* host, char* city, char* apiKey, char* language);//发送请求消息
+void wifiConfigInit();//wifi初始化配置
+void wifiRequest();//发送请求消息并获取数据JSON
 void readReponseContent();//读取响应消息的字符串数组指针
-//提取正文
+void monitorDisplay();//液晶显示
+void flip();
 
 void setup(void) {
-  /****************Wifi连接部分****************/
-  WiFi.mode(WIFI_STA);
   Serial.begin(BAUD_RATE);
+  /****************Wifi连接部分****************/
+  wifiConfigInit();
+  wifiRequest();
+  /****************液晶显示部分****************/
+  u8g2.begin();
+  u8g2.setContrast(0);//设置对比度
+  u8g2.enableUTF8Print();//使arduino支持UTF8输出
+  /****************温湿度获取部分****************/
+  dht.begin();
+//  flipper.attach(10, flip);//启用定时中断
+  lastTime = millis();
+}
+
+void loop(void) {
+  monitorDisplay();
+  if((millis()-lastTime)>weatherUpdateTime){
+    monitorDisplay();
+    wifiRequest();
+    lastTime = millis();
+    }
+}
+
+void wifiConfigInit(){
+  WiFi.mode(WIFI_STA);
   WiFi.begin(_ssid, _password);
   while (WiFi.status() != WL_CONNECTED) {
     //这个函数是wifi连接状态，返回wifi链接状态
@@ -60,32 +89,45 @@ void setup(void) {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());//WiFi.localIP()返回8266获得的ip地址
   client.setTimeout(HTTP_TIMEOUT);
+}
+
+void wifiRequest(){
+  if(WiFi.status() != WL_CONNECTED){
+    wifiConfigInit();
+  }
+//  wifiConfigInit();
   /*连接host地址*/
   while (!client.connected()){
      if (!client.connect(host, 80)){
          Serial.println("host connection....");
          delay(100);
+         return;
      }
   }
   /*发送请求消息*/
   client.print(GETUrl);
   /*读取响应消息*/
   delay(100);
-  int number = client.available();
   char Reponse[client.available()+1];//创建字符数组,加1是补'\0'转换成字符串数组
   memset(Reponse,0,client.available()+1);//清除数组内容
   readReponseContent(Reponse);
-  Serial.print("a:");
-  Serial.println(Reponse[number-1]);
-  /****************液晶显示部分****************/
-  u8g2.begin();
-  u8g2.setContrast(0);//设置对比度
-  u8g2.enableUTF8Print();//使arduino支持UTF8输出
-  /****************温湿度获取部分****************/
-  dht.begin();
+  client.stop();
+  Serial.println("closing connection");
+  }
+
+void readReponseContent(char Reponse[]) {
+  int idex = 0;
+  Serial.println("Get the data from Internet!");
+  while(client.available()){
+    Reponse[idex] = client.read();
+    idex++;
+  }
+  Reponse[idex] = '\0';//字符数组转字符串数组
+  Serial.println(Reponse);  
+  Serial.println("Read data Over!");
 }
 
-void loop(void) {
+void monitorDisplay(){
   u8g2.clearBuffer();
   u8g2.setFontDirection(0);//设置字体方向
   u8g2.clearBuffer();//清空缓存区
@@ -102,17 +144,8 @@ void loop(void) {
   u8g2.setFont(u8g2_font_crox5tb_tf);
   u8g2.print(((String)h+"%"));
   u8g2.sendBuffer();//写入缓存区
-  delay(1000);//显示1秒
 }
-
-void readReponseContent(char Reponse[]) {
-  int idex = 0;
-  Serial.println("Get the data from Internet!");
-  while(client.available()){
-    Reponse[idex] = client.read();
-    idex++;
-  }
-  Reponse[idex] = '\0';//字符数组转字符串数组
-  Serial.println(Reponse);  
-  Serial.println("Read data Over!");
+  
+void flip() {
+  wifiRequest();
 }
---
+++ Timer_DEMO/Timer_DEMO.ino
@@ -0,0 +1,36 @@
+/**
+ * 代码功能：板载LED开始0.3秒闪，然后快闪，最后常亮 
+ */
+
+#include <Ticker.h>
+
+Ticker flipper;
+
+int count = 0;
+
+void flip() {
+  int state = digitalRead(LED_BUILTIN);  // get the current state of GPIO1 pin
+  digitalWrite(LED_BUILTIN, !state);     // set pin to the opposite state
+
+  ++count;
+  // 当翻转次数达到20次的时候，切换led的闪烁频率，每隔0.1s翻转一次
+  if (count == 20) {
+    flipper.attach(0.1, flip);
+  }
+  // 当次数达到120次的时候关闭ticker
+  else if (count == 120) {
+    flipper.detach();
+  }
+}
+
+void setup() {
+  //LED_BUILTIN 对应板载LED的IO口
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  //每隔0.3s 翻转一下led状态
+  flipper.attach(0.3, flip);
+}
+
+void loop() {
+}
